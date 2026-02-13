164db4283df308276122368400327286c30db91d
Chirick <chirick86@users.noreply.github.com>
2026-02-14 00:14:15 +0100

    hello world

---
+++ Subtitles/ch_hello_world.lua
@@ -0,0 +1,22 @@
+-- @description Hello world
+-- @author Chirick
+-- @version 1.0.0
+-- @changelog
+--   + Initial release
+--   + test script to verify REAPER API access and console output
+--   + Added metadata and description for testing purposes
+-- @links
+--   Forum Thread       http://forum.cockos.com/showthread.php?t=169127
+--   Github repository  https://github.com/chirick86/reaperscripts
+--   Reddit thread      https://www.reddit.com/r/Reaper/
+-- @donation https://patreon.com/chirick
+-- @about
+--   # Hello world script
+--   
+--   Test script to verify REAPER API access and console output
+--   
+
+
+
+
+reaper.ShowConsoleMsg("Hello, world!\n")
