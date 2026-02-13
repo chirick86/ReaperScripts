b1cec78d944dc6c36c12d1da5953cd394ed6d82d
Chirick <chirick86@users.noreply.github.com>
2026-02-14 00:50:55 +0100

    Testing provides

---
+++ Subtitles/ch_icons_fonts.lua
@@ -0,0 +1,27 @@
+-- @description Icons and fonts
+-- @author Chirick
+-- @version 1.0.0
+-- @changelog
+--   + added ko-fi link
+--   + added provides section
+-- @provides
+--   ../icons_fonts_readme/*.*
+-- @links
+--   Forum Thread       http://forum.cockos.com/showthread.php?t=169127
+--   Github repository  https://github.com/chirick86/reaperscripts
+--   Reddit thread      https://www.reddit.com/r/Reaper/
+-- @donation
+--   https://patreon.com/chirick
+--   https://ko-fi.com/chirick
+-- @about
+--   # This script provides icons and fonts for Subtitle scripts.
+--   ## To use:
+--   1. Install the script.
+--   2. Copy the icons in Data\toolbar_icons Reaper's resource path (Options > Show REAPER resource path in explorer/finder).
+--   3. Install the fonts by double-clicking on them and clicking "Install" in the font viewer.
+--   4. Restart Reaper if it was open during installation.
+
+
+
+
+
